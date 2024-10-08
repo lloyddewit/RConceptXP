0cc99b8a076cc8f32947ddf636ea53a24634b0d9
Stephen Lloyd <stephen.lloyd03@gmail.com>
2024-10-08 10:51:21 +0200

    imported RConceptXP2 changes into RConceptXP

--- RConceptXP/ViewModels/MainViewModel.cs
+++ RConceptXP/ViewModels/MainViewModel.cs
@@ -1,6 +1,36 @@
-﻿namespace RConceptXP.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using RConceptXP.Views;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia;
+
+namespace RConceptXP.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public IRelayCommand OpenBoxplotCommand { get; }
+
+    public MainViewModel()
+    {
+        OpenBoxplotCommand = new RelayCommand(OpenBoxplot);
+    }
+
+    private async void OpenBoxplot()
+    {
+        // Check if Application or Application.Current is null
+        if (Application.Current == null || Application.Current.ApplicationLifetime == null)
+        {
+            return;
+        }
+
+        if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+        {
+            var mainWindow = desktopLifetime.MainWindow;
+            if (mainWindow != null)
+            {
+                var dialog = new Boxplot();
+                await dialog.ShowDialog(mainWindow);
+            }
+        }
+    }
+
 }
---
+++ RConceptXP/Views/Boxplot.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+
+namespace RConceptXP.Views;
+
+public partial class Boxplot : Window
+{
+    public Boxplot()
+    {
+        InitializeComponent();
+    }
+
+    private void OnToScriptClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+    }
+}
