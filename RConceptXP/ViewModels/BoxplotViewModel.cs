c3579bcbebb52e17794ead66d0f81b3a6716d19d
Stephen Lloyd <stephen.lloyd03@gmail.com>
2024-10-24 11:27:34 +0200

    refactored

--- RConceptXP/ViewModels/BoxplotViewModel.cs
+++ RConceptXP/ViewModels/BoxplotViewModel.cs
@@ -1,174 +1,121 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using RConceptXP.Views;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia;
-using CommunityToolkit.Mvvm.ComponentModel;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using Avalonia.Controls;
-using System;
+﻿using Avalonia.Controls;
 using Avalonia.Controls.Selection;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RConceptXP.Views;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
 
 namespace RConceptXP.ViewModels;
 
 //todo support empty list of columns (test that selector buttons enable/disable correctly and no crashes)
 public partial class BoxplotViewModel : ObservableObject
 {
+    public RelayCommand<TextBox> OnReceiverGotFocusCommand { get; }
+    public RelayCommand OnSelectorAddAllClickCommand { get; }
+    public RelayCommand OnSelectorAddClickCommand { get; }
+
+    public SelectionModel<string> Selection { get; }
+
     [ObservableProperty]
     private List<string> _columnNames; // List of data frame column names
 
     [ObservableProperty]
-    private string graphName; // Selected group to connect
+    private string _graphName; // Selected group to connect
 
     [ObservableProperty]
-    private List<string> graphNames; // List of groups to connect
-
-    [ObservableProperty]
-    private bool isGroupToConnectVisible; // Property to control ComboBox visibility
-
-    public RelayCommand<TextBox> SingleVariableGotFocusCommand { get; }
-    public RelayCommand<TextBox> MultipleVariableGotFocusCommand { get; }
-    public RelayCommand<TextBox> FactorGotFocusCommand { get; }
-    public RelayCommand<TextBox> SecondFactorGotFocusCommand { get; }
-    public RelayCommand<TextBox> FacetByGotFocusCommand { get; }
-    public RelayCommand OnSelectorAddClickCommand { get; }
-    public RelayCommand OnSelectorAddAllClickCommand { get; }
-
-    public SelectionModel<string> Selection { get; }
+    private List<string> _graphNames; // List of groups to connect
 
     // todo hardcoded column names for testing
     private static readonly List<string> ColumnNamesAll = new List<string> { "village", "field", "size", "fert", "variety", "yield", "fertgrp" };
     private static readonly List<string> ColumnNamesFactor = new List<string> { "village", "variety", "fertgrp" };
     private static readonly List<string> ColumnNamesNonFactor = new List<string> { "field", "size", "fert", "yield" };
 
-    private SelectorMediator selectorMediator;
+    private SelectorMediator _selectorMediator;
 
-    private ListBox columnsListBox;
-    private MenuItem addAllOption;
-    private TextBox factorTextBox;
+    private ListBox _columnsListBox;
+    private MenuItem _addAllOption;
+
+    private TextBox _singleVariableTextBox;
+    private TextBox _multipleVariableTextBox;
+    private TextBox _factorTextBox;
+    private TextBox _secondFactorTextBox;
+    private TextBox _facetByTextBox;
 
     public BoxplotViewModel(Boxplot boxplot)
     {
-        ColumnNames = ColumnNamesNonFactor; // Initialize list of data frame column names
-        GraphName = "plot1"; // Initialize selected group to connect
-        GraphNames = new List<string> { "box_plot", "jitter", "violin" }; // Initialize list of groups to connect
-        IsGroupToConnectVisible = false; // Initialize visibility state
-        SingleVariableGotFocusCommand = new RelayCommand<TextBox>(SingleVariableGotFocus);
-        MultipleVariableGotFocusCommand = new RelayCommand<TextBox>(MultipleVariableGotFocus);
-        FactorGotFocusCommand = new RelayCommand<TextBox>(FactorGotFocus);
-        SecondFactorGotFocusCommand = new RelayCommand<TextBox>(SecondFactorGotFocus);
-        FacetByGotFocusCommand = new RelayCommand<TextBox>(FacetByGotFocus);
+        // initialize receiver controls
+        OnReceiverGotFocusCommand = new RelayCommand<TextBox>(OnReceiverGotFocus);
+
+        _columnsListBox = boxplot.FindControl<ListBox>("columns") ?? throw new Exception("Cannot find columns ListBox by name");
+        _addAllOption = boxplot.FindControl<MenuItem>("addAllOption") ?? throw new Exception("Cannot find addAllOption MenuItem by name");
+        _singleVariableTextBox = boxplot.FindControl<TextBox>("singleVariableTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
+        _multipleVariableTextBox = boxplot.FindControl<TextBox>("multipleVariableTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
+        _factorTextBox = boxplot.FindControl<TextBox>("factorTextBox") ?? throw new Exception("Cannot find factor textBox by name");
+        _secondFactorTextBox = boxplot.FindControl<TextBox>("secondFactorTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
+        _facetByTextBox = boxplot.FindControl<TextBox>("facetByTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
+
+        // Note: We need to catch delete and backspace key presses in receivers so that the user
+        // can clear the receiver. There are simpler ways to catch most key events in Avalonia,
+        // but delete and backspace are a special case and require a Tunnel routing strategy.
+        _singleVariableTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel);
+        _multipleVariableTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel);
+        _factorTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel);
+        _secondFactorTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel);
+        _facetByTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel);
 
 
-        List<TextBox> receivers = new List<TextBox>
-        {
-            boxplot.FindControl<TextBox>("singleVariableTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name"),
-            boxplot.FindControl<TextBox>("multipleVariableTextBox") ?? throw new Exception("Cannot find multipleVariableTextBox by name"),
-            boxplot.FindControl<TextBox>("factorTextBox") ?? throw new Exception("Cannot find factorTextBox by name"),
-            boxplot.FindControl<TextBox>("secondFactorTextBox") ?? throw new Exception("Cannot find secondFactorTextBox by name"),
-            boxplot.FindControl<TextBox>("facetByTextBox") ?? throw new Exception("Cannot find facetByTextBox by name")
-        };
-
-        selectorMediator = new SelectorMediator(receivers);
-
+        // initialize selector controls
+        ColumnNames = ColumnNamesNonFactor;
         OnSelectorAddClickCommand = new RelayCommand(OnSelectorAddClick);
         OnSelectorAddAllClickCommand = new RelayCommand(OnSelectorAddAllClick);
-
         Selection = new SelectionModel<string>();
 
-        columnsListBox = boxplot.FindControl<ListBox>("columns") ?? throw new Exception("Cannot find columns ListBox by name");
-        addAllOption = boxplot.FindControl<MenuItem>("addAllOption") ?? throw new Exception("Cannot find addAllOption MenuItem by name");
-        TextBox singleVariableTextBox = boxplot.FindControl<TextBox>("singleVariableTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
-        TextBox multipleVariableTextBox = boxplot.FindControl<TextBox>("multipleVariableTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
-        factorTextBox = boxplot.FindControl<TextBox>("factorTextBox") ?? throw new Exception("Cannot find factor textBox by name");
-        TextBox secondFactorTextBox = boxplot.FindControl<TextBox>("secondFactorTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
-        TextBox facetByTextBox = boxplot.FindControl<TextBox>("facetByTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
+        // initialize selector model
+        List<TextBox> receivers = new List<TextBox>
+        {
+            _singleVariableTextBox,
+            _multipleVariableTextBox,
+            _factorTextBox,
+            _secondFactorTextBox,
+            _facetByTextBox
+        };
+        _selectorMediator = new SelectorMediator(receivers);
 
-        singleVariableTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel); // Use AddHandler with RoutingStrategies.Tunnel
-        multipleVariableTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel); // Use AddHandler with RoutingStrategies.Tunnel
-        factorTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel); // Use AddHandler with RoutingStrategies.Tunnel
-        secondFactorTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel); // Use AddHandler with RoutingStrategies.Tunnel
-        facetByTextBox.AddHandler(InputElement.KeyDownEvent, OnReceiverKeyDown, RoutingStrategies.Tunnel); // Use AddHandler with RoutingStrategies.Tunnel
-
-        //// ensure initial input focus is the single variable text box
-        //TextBox singleVariableTextBox = boxplot.FindControl<TextBox>("singleVariableTextBox") ?? throw new Exception("Cannot find singleVariableTextBox by name");
-        //singleVariableTextBox.AttachedToVisualTree += (s, e) => singleVariableTextBox.Focus();
+        // intitialize graph name autocomplete box
+        GraphName = "plot1"; // Initialize selected group to connect
+        GraphNames = new List<string> { "box_plot", "jitter", "violin" }; // Initialize list of groups to connect
     }
 
-    private void SingleVariableGotFocus(TextBox receiver)
+    private void OnReceiverGotFocus(TextBox? receiver)
     {
-        selectorMediator.SetFocus(receiver);
-        SetColumnNamesNonFactor();
-        columnsListBox.SelectionMode = SelectionMode.Single | SelectionMode.AlwaysSelected | SelectionMode.Toggle;
-        addAllOption.IsEnabled = false;
-    }
+        ArgumentNullException.ThrowIfNull(receiver, nameof(receiver));
 
-    private void MultipleVariableGotFocus(TextBox receiver)
-    {
-        selectorMediator.SetFocus(receiver);
-        SetColumnNamesNonFactor();
-        columnsListBox.SelectionMode = SelectionMode.Multiple | SelectionMode.AlwaysSelected | SelectionMode.Toggle;
-        addAllOption.IsEnabled = true;
-        factorTextBox.Text = "";
-    }
+        _selectorMediator.SetFocus(receiver);
+        if (receiver == _factorTextBox)
+        {
+            ColumnNames = new List<string>(ColumnNamesFactor);
+            ColumnNames.AddRange(ColumnNamesNonFactor);
+        }
+        else if (receiver == _secondFactorTextBox || receiver == _facetByTextBox)
+            ColumnNames = ColumnNamesFactor;
+        else
+            ColumnNames = ColumnNamesNonFactor;
 
-    private void FactorGotFocus(TextBox receiver)
-    {
-        selectorMediator.SetFocus(receiver);
-        SetColumnNamesAll();
-        columnsListBox.SelectionMode = SelectionMode.Single | SelectionMode.AlwaysSelected | SelectionMode.Toggle;
-        addAllOption.IsEnabled = false;
-    }
-
-    private void SecondFactorGotFocus(TextBox receiver)
-    {
-        selectorMediator.SetFocus(receiver);
-        SetColumnNamesFactor();
-        columnsListBox.SelectionMode = SelectionMode.Single | SelectionMode.AlwaysSelected | SelectionMode.Toggle;
-        addAllOption.IsEnabled = false;
-    }
-
-    private void FacetByGotFocus(TextBox receiver)
-    {
-        selectorMediator.SetFocus(receiver);
-        SetColumnNamesFactor();
-        columnsListBox.SelectionMode = SelectionMode.Single | SelectionMode.AlwaysSelected | SelectionMode.Toggle;
-        addAllOption.IsEnabled = false;
-    }
-
-    private void OnSelectorAddClick()
-    {
-        string selectedValue = Selection.SelectedItem ?? throw new Exception("Selected value in column selector list is null");
-        IReadOnlyList<string?> selectedItems = Selection.SelectedItems;
-        selectorMediator.AddSelectedValueToReceiver(selectedItems);
-    }
-
-    private void OnSelectorAddAllClick()
-    {
-        Selection.SelectAll();
-        IReadOnlyList<string?> selectedItems = Selection.SelectedItems;
-        selectorMediator.AddSelectedValueToReceiver(selectedItems);
-    }
-
-    private void SetColumnNamesAll()
-    {
-        ColumnNames = new List<string>(ColumnNamesFactor);
-        ColumnNames.AddRange(ColumnNamesNonFactor);
-        // Additional logic using textBlock if needed
-    }
-
-    private void SetColumnNamesFactor()
-    {
-        ColumnNames = ColumnNamesFactor;
-        // Additional logic using textBlock if needed
-    }
-
-    private void SetColumnNamesNonFactor()
-    {
-        ColumnNames = ColumnNamesNonFactor;
-        // Additional logic using textBlock if needed
+        _columnsListBox.SelectionMode = SelectionMode.AlwaysSelected | SelectionMode.Toggle;
+        if (receiver == _multipleVariableTextBox)
+        {
+            _addAllOption.IsEnabled = true;
+            _columnsListBox.SelectionMode |= SelectionMode.Multiple;
+        }
+        else
+        {
+            _columnsListBox.SelectionMode |= SelectionMode.Single;
+            _addAllOption.IsEnabled = false;
+        }
     }
 
     private void OnReceiverKeyDown(object? sender, KeyEventArgs e)
@@ -180,5 +127,19 @@ public partial class BoxplotViewModel : ObservableObject
                 textBox.Clear();
             }
         }
+    }
+
+    private void OnSelectorAddAllClick()
+    {
+        Selection.SelectAll();
+        IReadOnlyList<string?> selectedItems = Selection.SelectedItems;
+        _selectorMediator.AddSelectedValueToReceiver(selectedItems);
+    }
+
+    private void OnSelectorAddClick()
+    {
+        string selectedValue = Selection.SelectedItem ?? throw new Exception("Selected value in column selector list is null");
+        IReadOnlyList<string?> selectedItems = Selection.SelectedItems;
+        _selectorMediator.AddSelectedValueToReceiver(selectedItems);
     }
 }
