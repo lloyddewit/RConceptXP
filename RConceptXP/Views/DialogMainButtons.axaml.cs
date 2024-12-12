8292943576925be0c3f5d1cdd337b61bbe20212f
Stephen Lloyd <stephen.lloyd03@gmail.com>
2024-12-12 10:47:59 +0100

    moved main dialog buttons to separate control

---
+++ RConceptXP/Views/DialogMainButtons.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RConceptXP.Views;
+
+public partial class DialogMainButtons : UserControl
+{
+    public DialogMainButtons()
+    {
+        InitializeComponent();
+    }
+}
