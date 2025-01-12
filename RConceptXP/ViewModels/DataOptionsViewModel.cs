91e79bef33db234c2a391f7994ca56b9dc41c05c
Stephen Lloyd <stephen.lloyd03@gmail.com>
2025-01-12 10:13:38 +0100

    removed unused variable

--- RConceptXP/ViewModels/DataOptionsViewModel.cs
+++ RConceptXP/ViewModels/DataOptionsViewModel.cs
@@ -5,10 +5,6 @@ namespace RConceptXP.ViewModels;
 //todo support empty list of columns (test that selector buttons enable/disable correctly and no crashes)
 public partial class DataOptionsViewModel : ObservableObject
 {
-    [ObservableProperty]
-    private int _selectedTabIndex;
-
-
     public DataOptionsViewModel()
     {
     }
