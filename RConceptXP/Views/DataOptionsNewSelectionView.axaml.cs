af87734f2fc6df06eee2995e15acca578643b70c
Stephen Lloyd <stephen.lloyd03@gmail.com>
2025-01-16 10:11:38 +0100

    added initial view for data options new selection

---
+++ RConceptXP/Views/DataOptionsNewSelectionView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RConceptXP.Views;
+
+public partial class DataOptionsNewSelectionView : UserControl
+{
+    public DataOptionsNewSelectionView()
+    {
+        InitializeComponent();
+    }
+}
